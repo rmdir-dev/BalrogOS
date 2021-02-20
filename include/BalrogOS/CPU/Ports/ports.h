5778d99feaf78bd2aaa973e2b6ae81d6717f3942
rmdir-dev <stonsphilippe@gmail.com>
2021-02-20 17:26:05 +0100

    [PORTS] port as inlined code

--- include/BalrogOS/CPU/Ports/ports.h
+++ include/BalrogOS/CPU/Ports/ports.h
@@ -8,7 +8,15 @@
  * @param port port address
  * @param value value (in byte) to send
  */
-void out_byte(uint16_t port, uint8_t value);
+static inline void out_byte(uint16_t port, uint8_t value)
+{
+    /*
+    * outb = out byte
+    * "a" = set value into rax register
+    * "Nd" = N constraint d rdx register
+    */
+    asm volatile("outb %0, %1": : "a"(value), "Nd"(port));
+}
 
 /**
  * @brief get the value (in byte) from a port
@@ -16,7 +24,17 @@ void out_byte(uint16_t port, uint8_t value);
  * @param port port address
  * @return uint8_t value send by the port
  */
-uint8_t in_byte(uint16_t port);
+static inline uint8_t in_byte(uint16_t port)
+{
+    uint8_t ret;
+    /*
+    * inb = in byte
+    * "=a" = output constraint to rax, and set rax into ret
+    * "Nd" = N constraint d rdx register
+    */
+    asm volatile("inb %1, %0" : "=a" (ret) : "Nd" (port));
+    return ret;
+}
 
 /**
  * @brief send a word to a port
@@ -24,7 +42,15 @@ uint8_t in_byte(uint16_t port);
  * @param port port address
  * @param value value (in word) to send
  */
-void out_word(uint16_t port, uint16_t value);
+static inline void out_word(uint16_t port, uint16_t value)
+{
+    /*
+    * outw = out word
+    * "a" = set value into rax register
+    * "Nd" = N constraint d rdx register
+    */
+    asm volatile("outw %0, %1": : "a"(value), "Nd"(port));
+}
 
 /**
  * @brief get a value from a port
@@ -32,4 +58,14 @@ void out_word(uint16_t port, uint16_t value);
  * @param port port address
  * @return uint16_t value send by the port
  */
-uint16_t in_word(uint16_t port);
+static inline uint16_t in_word(uint16_t port)
+{
+    uint16_t ret;
+    /*
+    * inw = in word
+    * "=a" = output constraint to rax, and set rax into ret
+    * "Nd" = N constraint d rdx register
+    */
+    asm volatile("inw %1, %0" : "=a" (ret) : "Nd" (port));
+    return ret;
+}

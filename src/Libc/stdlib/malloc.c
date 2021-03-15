c0cc56ac3ec47c5527fc566e52ca95dfec291fe7
rmdir-dev <stonsphilippe@gmail.com>
2021-03-15 13:34:53 +0100

    [LIBC] correct some bugs

---
+++ src/Libc/stdlib/malloc.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+void* malloc(size_t memory_size)
+{
+    return 0;
+}
+
+void free(void* pointer)
+{
+
+}
--- src/Libc/string/memcmp.c
+++ src/Libc/string/memcmp.c
@@ -1,11 +1,11 @@
 #include <string.h>
 
-
 int memcmp(const void* addr_1, const void* addr_2, size_t byte_size)
 {
     const unsigned char* ptr_1 = addr_1;
     const unsigned char* ptr_2 = addr_2;
-    for(size_t i; i < byte_size; i++)
+
+    for(size_t i = 0; i < byte_size; i++)
     {
         if(ptr_1[i] < ptr_2[i])
         {

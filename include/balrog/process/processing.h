9f88a72c24a4bd2a9b4c298da400807fecfbf027
rmdir-dev <stonsphilippe@gmail.com>
2021-10-26 22:12:25 +0200

    [SYSCALL] kill syscall

---
+++ include/balrog/process/processing.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/* Historical signals specified by POSIX. */
+#define	SIGHUP		1	/* Hangup.  */
+#define	SIGQUIT		3	/* Quit.  */
+#define	SIGTRAP		5	/* Trace/breakpoint trap.  */
+#define	SIGKILL		9	/* Killed.  */
+#define	SIGPIPE		13	/* Broken pipe.  */
+#define	SIGALRM		14	/* Alarm clock.  */
--- src/Kernel/Syscall/ProcessManagement/sys_kill.c
+++ src/Kernel/Syscall/ProcessManagement/sys_kill.c
@@ -1,7 +1,22 @@
 #include "BalrogOS/Syscall/syscall.h"
 #include "BalrogOS/CPU/Interrupts/interrupt.h"
+#include "BalrogOS/Tasking/process.h"
+#include "balrog/process/processing.h"
 
 int sys_kill(interrupt_regs* stack_frame)
 {
+    process* proc = proc_get_process(stack_frame->rdi);
+    if(proc)
+    {
+        switch (stack_frame->rsi)
+        {
+        case SIGKILL:
+            proc_kill(proc);
+            break;
+        
+        default:
+            break;
+        }
+    }
     return 0;
 }
